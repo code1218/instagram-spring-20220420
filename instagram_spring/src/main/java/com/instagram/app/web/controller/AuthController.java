6fee17816601ee69c70798dc07ebbf18c5f1f2ad
ITPS <ITPS@gmail.com>
2022-04-21 11:56:30 +0900

    마이바티스 연결

--- instagram_spring/src/main/java/com/instagram/app/web/controller/AuthController.java
+++ instagram_spring/src/main/java/com/instagram/app/web/controller/AuthController.java
@@ -1,30 +1,34 @@
 package com.instagram.app.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.instagram.app.web.dto.auth.SignupRequestDto;
+import com.instagram.app.service.AuthService;
 
 @Controller
 public class AuthController {
+	
+	@Autowired 
+	private AuthService authService;
+	
+	
 	@RequestMapping(value = "/auth/signup", method = RequestMethod.GET)
-	public String getSignup(SignupRequestDto signupRequestDto) {
-		
-		System.out.println("phoneOrEmail: " + signupRequestDto.getPhoneOrEmail());
-		System.out.println("name: " + signupRequestDto.getName());
-		System.out.println("username: " + signupRequestDto.getUsername());
-		System.out.println("password: " + signupRequestDto.getPassword());
-		
+	public String getSignup() {
 		return "auth/signup";
 	}
 	
 	@ResponseBody
 	@RequestMapping(value = "/auth/username/check", method = RequestMethod.GET)
-	public String usernameCheck(String username) {
-		System.out.println(username);
-		return username;
+	public boolean usernameCheck(String username) {
+		return authService.checkUsername(username);
 	}
 }
+
+
+
+
+
+
