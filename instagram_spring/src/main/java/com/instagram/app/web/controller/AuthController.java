3a796fb552bbcaa2b25047b0cd862cd04120e5fb
ITPS <ITPS@gmail.com>
2022-04-21 09:44:16 +0900

    인스타그램 회원가입 시작

--- instagram_spring/src/main/java/com/instagram/app/web/controller/AuthController.java
+++ instagram_spring/src/main/java/com/instagram/app/web/controller/AuthController.java
@@ -3,11 +3,20 @@ package com.instagram.app.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.instagram.app.web.dto.auth.SignupRequestDto;
 
 @Controller
 public class AuthController {
 	@RequestMapping(value = "/auth/signup", method = RequestMethod.GET)
-	public String getSignup() {
+	public String getSignup(SignupRequestDto signupRequestDto) {
+		
+		System.out.println("phoneOrEmail: " + signupRequestDto.getPhoneOrEmail());
+		System.out.println("name: " + signupRequestDto.getName());
+		System.out.println("username: " + signupRequestDto.getUsername());
+		System.out.println("password: " + signupRequestDto.getPassword());
+		
 		return "auth/signup";
 	}
 }
